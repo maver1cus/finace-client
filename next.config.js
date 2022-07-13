56ea8730003c1a33e346d37c86e2cc18190bda56
Sergey Petrov <mavericusdev@gmail.com>
2022-07-13 12:00:16 +0300

    settings next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const api = process.env.API_HOST || 'htpp://localhost:3010';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  rewrites: [{ source: '/api/:path*', destination: `${api}/:path*` }],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
